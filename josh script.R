600d79a8f8855e4ab26d764a4dc0cac1cf6b65a2
josh-grosman <37800697+josh-grosman@users.noreply.github.com>
2018-10-06 11:56:15 +1000

    feature selection

--- josh script.R
+++ josh script.R
@@ -9,22 +9,63 @@ zomato<-readRDS("zomato_phase2.rds")
 #rating text (base=poor)
 zomato$Rating.text<-factor(zomato$Rating.text, 
                            levels = c("Poor", "Average", "Good","Very Good", "Excellent")) 
-levels(zomato$Rating.text)
 
 #continent (base=rest of world)
 zomato$continent<-factor(zomato$continent, 
                            levels = c("Rest of World", "Europe", "Asia","North America", "Oceania")) 
-levels(zomato$continent)
+
+
+#========= Feature selection =========#
+
+library(glmulti)
+#--bic--#
+set.seed(123)
+search.1.bic <- glmulti(y = Rating.text ~ ., 
+                         data = zomato, 
+                         fitfunction = "glm", 
+                         level = 1, 
+                         method = "g", 
+                         marginality = TRUE,
+                         crit = "bic", 
+                         family = binomial(link = "logit"))
+#works for multinomial response?
+
+print(search.1.aicc)
+aa <- weightable(search.1.aicc)
+cbind(model = aa[1:5,1], round(aa[1:5,2:3], digits = 3))
+
+plot(search.1.bic, type = "p")
+
+#picks votes and has online delivery as only predictors...
+
+#--aicc--#
+set.seed(123)
+search.1.aicc <- glmulti(y = Rating.text ~ ., 
+                         data = zomato, 
+                         fitfunction = "glm", 
+                         level = 1, 
+                         method = "g", 
+                         marginality = TRUE,
+                         crit = "aicc", 
+                         family = binomial(link = "logit"))
+
+print(search.1.aicc)
+aa <- weightable(search.1.aicc)
+cbind(model = aa[1:5,1], round(aa[1:5,2:3], digits = 3))
+
+plot(search.1.aicc, type = "p")
+
+#picks has online delivery, votes, continent, cuisine and cuisine range
 
 #========= Modelling =========#
 
 #nominal
-fit.full<-multinom(formula = Rating.text ~ Has.Table.booking + Has.Online.delivery  + Votes + Average.Cost.for.two.Std + continent +
+fit.test<-multinom(formula = Rating.text ~ Has.Online.delivery  + Votes +  continent + cuisine+
                  Cuisine_Range, data=zomato)
 
-summary(fit.full)
+summary(fit.test)
 
-Anova(fit.full)
+Anova(fit.test)
 
 #all predictors besides cuisine range is highly significant, while cuisine range is moderatly significant
 
