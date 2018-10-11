a17c1a1980d89c5ac553a53d95300a9d0fe08c64
josh-grosman <37800697+josh-grosman@users.noreply.github.com>
2018-10-11 12:46:47 +1100

    prediction plots

--- josh script.R
+++ josh script.R
@@ -52,6 +52,8 @@ cbind(model = aa[1:5,1], round(aa[1:5,2:3], digits = 3))
 
 plot(search.1way.bic, type = "p")
 
+#saveRDS(search.1way.bic, "Feature Selection/search.1way.bic.rds")
+
 #picks votes and has online delivery as only predictors...
 
 # 2-way interactions
@@ -114,20 +116,39 @@ plot(search.2way.aicc, type = "p")
 #========= Modelling =========#
 
 #nominal
-fit.test<-multinom(formula = Rating.text ~ Has.Online.delivery  + Votes +  continent + cuisine+
-                 Cuisine_Range, data=zomato.train)
+fit.bic.nom<-multinom(formula = Rating.text ~ Has.Online.delivery  + Votes, data=zomato)
 
-summary(fit.test)
+
+summary(fit.bic.nom)
 
 Anova(fit.test)
 
 #all predictors besides cuisine range is highly significant, while cuisine range is moderatly significant
 
 #ordinal
-fit.full.ord<-polr(formula = Rating.text ~ Has.Table.booking + Has.Online.delivery  + Votes + Average.Cost.for.two.Std + continent +
-                     Cuisine_Range, data=zomato.train,
+fit.bic.ord<-polr(formula = Rating.text ~ Has.Online.delivery  + Votes, data=zomato,
                   method = "logistic")
+
+saveRDS(fit.bic.ord,"Models/bic_ord_TRAIN.rds")
 
 summary(fit.full.ord)
 
 Anova(fit.full.ord)
+
+#plot
+
+#prediction dataframe
+dvotes <- data.frame(Has.Online.delivery = rep(c("Yes", "No"), each = 50), 
+                     Votes = rep(c(5, 125, 255, 505, 755, 1005, 1205, 1505, 2005, 2505, 3005, 3505, 
+                                   4005, 4505, 5005, 5505, 6005, 6505, 7005,
+                                   7505, 8005, 8505, 9005, 9505, 10005), 4))
+
+#generate predictions using model
+pp.votes <- cbind(dvotes, predict(fit.bic.nom, newdata = dvotes, type = "probs", se = TRUE))
+#melt for visualisation
+lpp <- melt(pp.votes, id.vars = c("Has.Online.delivery", "Votes"), value.name = "Probability")
+
+
+ggplot(lpp, aes(x = Votes, y = Probability, colour = Has.Online.delivery)) + 
+  geom_line() + facet_grid(variable ~ ., scales = "free") + theme_minimal()+
+  labs(title="Probability of Rating Levels by Votes and Online Delivery")
